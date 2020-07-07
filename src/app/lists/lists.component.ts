57f637db7ca9071965e58f89c199b5fad7c379f3
SofiaVP <sofiavayas@hotmail.com>
2020-07-07 02:35:15 +0200

    new tab added

--- src/app/lists/lists.component.ts
+++ src/app/lists/lists.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 
 
@@ -8,8 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lists.component.scss']
 })
 export class ListsComponent implements OnInit {
+  tabs = ['Clothes', 'toiletries', 'Entretainment'];
+  selected = new FormControl(0);
+  tabName: string;
 
- 
+  addTab(tabName: string) {
+    this.tabs.push(tabName.valueOf());
+  }
+
+  removeTab(index: number) {
+    this.tabs.splice(index, 1);
+  }
+
   constructor() { }
 
   ngOnInit(): void {
